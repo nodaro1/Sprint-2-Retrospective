f9a986c8a8815773c4e631709fcb214dc910a463
Benjamin Cohen <65982703+OfficialBenjaminCohen@users.noreply.github.com>
2024-10-12 16:04:42 -0700

    Update ARLevelSelector.cs
    Add each level to the home screen

--- ARLevelSelector.cs
+++ ARLevelSelector.cs
@@ -1,5 +1,3 @@
-//C# Unity Tutorial Boilerplate Code
-
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -7,30 +5,35 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class VRLevelSelector : MonoBehaviour
 {
     [SerializeField] private GameObject levelSelectorPanel;
-    [SerializeField] private Button[] levelButtons;
+    [SerializeField] private Button[] levelButtons; // assign these in inspector
     [SerializeField] private Button viewResultsButton;
     [SerializeField] private XRRayInteractor rayInteractor;
 
     private void Start()
     {
         InitializeLevelButtons();
+        InitializeOtherButtons();
     }
 
     private void InitializeLevelButtons()
     {
-        for (int i = 0; i < levelButtons.Length; i++)
+        string[] gameNames = { "Mastermind", "Spot the difference", "Stacking Challenge" };
+
+        for (int i = 0; i < levelButtons.Length && i < gameNames.Length; i++)
         {
-            int levelIndex = i + 1;
-            levelButtons[i].GetComponentInChildren<Text>().text = $"Level {levelIndex}";
-            levelButtons[i].onClick.AddListener(() => LoadLevel(levelIndex));
+            // set button text to the game name
+            levelButtons[i].GetComponentInChildren<Text>().text = gameNames[i];
+
+            int index = i; // retrieve index for the closure
+            levelButtons[i].onClick.AddListener(() => LoadLevel(gameNames[index]));
         }
     }
 
     private void InitializeOtherButtons()
     {
-        // Add more buttons for games
-        viewResultsButton.GetComponentInChildren<Text>().text = $"View Results";
-        viewResultsButton.onClick.AddListener(() => ViewResults());
+        // initializing "View Results" button
+        viewResultsButton.GetComponentInChildren<Text>().text = "View Results";
+        viewResultsButton.onClick.AddListener(ViewResults);
     }
 
     public void ShowLevelSelector()
@@ -41,20 +44,19 @@ public class VRLevelSelector : MonoBehaviour
     public void HideLevelSelector()
     {
         levelSelectorPanel.SetActive(false);
-        // Makes sure patient checks with therapist to understand the results due to brain trauma
-        Debug.Log($"Consult With Therapist To Understand Progress");
+        Debug.Log("Consult With Therapist To Understand Progress");
     }
 
-    private void LoadLevel(int levelIndex)
+    private void LoadLevel(string gameName)
     {
-        Debug.Log($"Loading Level {levelIndex}");
-        // Add levels
+        Debug.Log($"Loading {gameName}");
+        // code to load the specific game scene or start the game logic
         HideLevelSelector();
     }
 
     private void ViewResults()
     {
-        Debug.Log($"View of Patient Results");
-        // Add details once results have been calculated
+        Debug.Log("Viewing Patient Results");
+        // code to display results
     }
 }
